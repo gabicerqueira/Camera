c26052c9ca3332e4f909b8f7579766feac8241e2
gabicerqueira <133364342+gabicerqueira@users.noreply.github.com>
2023-11-24 15:24:50 -0300

    Initial commit

---
+++ Tarefa - camera/camera.js
@@ -0,0 +1,96 @@
+var mediaStream;
+var fotos = [];
+
+function abrirCamera(){
+    navigator.mediaDevices.getUserMedia({ video: true, audio: false})
+    .then(function (stream){
+        mediaStream = stream;
+
+        const areaVideo = document.getElementById('camera');
+        areaVideo.srcObject = stream;
+        areaVideo.style.display = 'block';
+    })
+    .catch(function (error){
+        console.error('Erro ao acessar a câmera:', error)
+    });
+}
+
+function tirarFoto() {
+    const areaVideo = document.getElementById('camera');
+    const canvas = document.createElement('canvas');
+    canvas.width = areaVideo.videoWidth;
+    canvas.height = areaVideo.videoHeight;
+    const context = canvas.getContext('2d');
+    context.drawImage(areaVideo, 0, 0, canvas.width, canvas.height);
+
+    const legenda = prompt('Digite a legenda para a foto:'); // Solicita uma legenda
+
+    const imageDataUrl = canvas.toDataURL(); // convertendo a imagem para o formato base64
+
+    fotos.push({ url: imageDataUrl, legenda: legenda }); // adiciona a foto ao array com a legenda
+
+    exibirFotos(); // exibe todas as fotos na div 'foto'
+}
+
+function exibirFotos() {
+    const fotoDiv = document.getElementById('foto');
+    fotoDiv.innerHTML = '';
+
+    fotos.forEach(function (foto, index) {
+        const containerDiv = document.createElement('div');
+        containerDiv.classList.add('foto-container');
+
+        const img = document.createElement('img');
+        img.src = foto.url;
+        img.alt = `Foto ${index + 1}`;
+        img.classList.add('foto-item');
+
+        img.addEventListener('click', function() {
+            const downloadLink = document.createElement('a');
+            downloadLink.href = foto.url;
+            downloadLink.download = `foto_${index + 1}.png`;
+
+            downloadLink.click();
+            document.body.removeChild(downloadLink);
+        });
+
+        containerDiv.appendChild(img);
+
+        if (foto.legenda) {
+            const legendaP = document.createElement('p');
+            legendaP.classList.add('legenda');
+            legendaP.textContent = foto.legenda;
+            containerDiv.appendChild(legendaP);
+        }
+
+        const lixoIcon = document.createElement('img');
+        lixoIcon.src = 'img/lixeira.png';
+        lixoIcon.alt = 'Excluir';
+        lixoIcon.classList.add('lixo-icon');
+        lixoIcon.addEventListener('click', function () {
+            excluirFoto(index);
+        });
+        containerDiv.appendChild(lixoIcon);
+
+        fotoDiv.appendChild(containerDiv);
+    });
+}
+
+function excluirFoto(index) {
+    fotos.splice(index, 1);
+    exibirFotos();
+}
+
+function fechar() {
+    const fotoDiv = document.getElementById('foto');
+    fotoDiv.style.marginTop = '60px'
+
+    navigator.mediaDevices.getUserMedia({ video: false });
+    const areaVideo = document.getElementById('camera');
+    areaVideo.srcObject = null;
+    mediaStream = null;
+    areaVideo.style.display = 'none';
+}
+
+const foto = document.getElementById('foto');
+
---
+++ script.js
@@ -0,0 +1,42 @@
+var mediaStream;
+
+function abrirCamera(){
+    navigator.mediaDevices.getUserMedia({ video: true, audio: false})
+    .then(function (stream){
+        mediaStream = stream;
+
+        const areaVideo = document.getElementById('camera');
+        areaVideo.srcObject = stream;
+    })
+    .catch(function (error){
+        console.error('Erro ao acessar a câmera:', error)
+    });
+}
+
+function tirarFoto(){
+    const areaVideo = document.getElementById('camera');
+    const canvas = document.createElement('canvas');
+    canvas.width = areaVideo.videoWidth;
+    canvas.height = areaVideo.videoHeight;
+    const context = canvas.getContext('2d');
+    context.drawImage(areaVideo, 0, 0, canvas.width, canvas.height);
+
+    const imageDataUrl = canvas.toDataURL(); //convertendo a imagem para o formato base64
+
+    const fotoDiv = document.getElementById('foto'); //armazena a foto
+    fotoDiv.style.backgroundImage = `url(${imageDataUrl})`;
+
+    const downloadLink = document.createElement('a');
+
+    downloadLink.href = imageDataUrl;
+    downloadLink.download = 'foto.png';
+    downloadLink.textContent = 'Clique para baixar';
+    document.body.appendChild(downloadLink);
+}
+
+function fechar(){
+    navigator.mediaDevices.getUserMedia({video: false});
+    const areaVideo = document.getElementById('camera');
+    areaVideo.srcObject = null;
+    mediaStream = null;
+}
